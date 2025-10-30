18d3cb1dd0df0f2a2dfa12f3a6af1320de2727a5
obedab <obedabiragiye@gmail.com>
2025-10-30 16:41:15 +0200

    Initialization of my projects

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <div
+      className="min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: 'url(/bg-image.png)' }}
+    >
+      <h1 className="text-white text-4xl font-bold text-center pt-40">
+       Successfully setup of my project!
+      </h1>
+    </div>
+  );
+}
+
